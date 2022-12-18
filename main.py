12e17692e1812be694a77d9ce58983f6fa53f79a
J Cat <117458947+jcat-github@users.noreply.github.com>
2022-12-18 07:57:47 +0000

    ok

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-special_dict = {["yes", "meow"]: "hi"}
+
 user_message = [
 	["print", "python"],
 	["print", "JavaScript"],
@@ -9,6 +9,17 @@ bot_reply = [
 	"console.log(\"print\")",
 	"input(\"input\")",
 ]
-var = input("type")
-if var in special_dict:
-	print("seccess")
+var = input("search: ").lower()
+idx = 0
+for i in user_message:
+	contains = 0
+	for x in i:
+		if x in var:
+			contains += 1
+	if contains == len(i):
+		print(bot_reply[idx])
+		break
+	idx += 1
+
+if idx == len(bot_reply): print("nothing was matching.")
+import main
